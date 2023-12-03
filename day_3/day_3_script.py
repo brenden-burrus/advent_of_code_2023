56fe63224d78780dcdad526e4730573e3f4aa036
brenden burrus <brenden.burrus@gmail.com>
2023-12-03 15:39:18 -0600

    uncommenting part 1

--- day_3/day_3_script.py
+++ day_3/day_3_script.py
@@ -6,32 +6,32 @@ with open("day_3_input.txt") as file_contents:
 ###################################################################
 
 
-# coordinates = set()
+coordinates = set()
 
 
-# for row_num, row in enumerate(input):
-#     for col_num, col_content in enumerate(row):
-#         if col_content == "." or col_content.isdigit():
-#             continue
-#         for adj_rows in [row_num - 1, row_num, row_num + 1]:
-#             for adj_cols in [col_num -1, col_num, col_num + 1]:
-#                 if adj_rows < 0 or adj_rows >= len(input) or adj_cols < 0 or adj_cols >= len(input[adj_rows]) or not input[adj_rows][adj_cols].isdigit():
-#                     continue
-#                 while adj_cols > 0 and input[adj_rows][adj_cols-1].isdigit():
-#                     adj_cols -= 1
-#                 coordinates.add((adj_rows, adj_cols))
+for row_num, row in enumerate(input):
+    for col_num, col_content in enumerate(row):
+        if col_content == "." or col_content.isdigit():
+            continue
+        for adj_rows in [row_num - 1, row_num, row_num + 1]:
+            for adj_cols in [col_num -1, col_num, col_num + 1]:
+                if adj_rows < 0 or adj_rows >= len(input) or adj_cols < 0 or adj_cols >= len(input[adj_rows]) or not input[adj_rows][adj_cols].isdigit():
+                    continue
+                while adj_cols > 0 and input[adj_rows][adj_cols-1].isdigit():
+                    adj_cols -= 1
+                coordinates.add((adj_rows, adj_cols))
 
-# number_set = []
+number_set = []
 
-# for r, c in coordinates:
-#     num = ""
-#     while c < len(input[r]) and input[r][c].isdigit():
-#         num += input[r][c]
-#         c += 1
-#     if num != "":
-#         number_set.append(int(num))
+for r, c in coordinates:
+    num = ""
+    while c < len(input[r]) and input[r][c].isdigit():
+        num += input[r][c]
+        c += 1
+    if num != "":
+        number_set.append(int(num))
 
-# print(f"part 1 value = {sum(number_set)}")
+print(f"part 1 value = {sum(number_set)}")
 
 
 ###################################################################
